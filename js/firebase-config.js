6591164f8f02f1511e4079fa2a93a105423d3d86
lahsanaskouk-png <lahsanaskouk@gmail.com>
2025-12-19 20:35:25 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // إعدادات Firebase - يجب استبدالها بإعدادات مشروعك
 const firebaseConfig = {
-    apiKey: "AIzaSyBmOOERF0KkDQ7G08L7_F7TuM7SItkoO_o",
-    authDomain: "brtiol.firebaseapp.com",
-    projectId: "brtiol",
-    storageBucket: "brtiol.firebasestorage.app",
-    messagingSenderId: "711908351848",
-    appId: "1:711908351848:web:a6c1ac4836988c4a7340dd"
+  apiKey: "AIzaSyBmOOERF0KkDQ7G08L7_F7TuM7SItkoO_o",
+  authDomain: "brtiol.firebaseapp.com",
+  projectId: "brtiol",
+  storageBucket: "brtiol.firebasestorage.app",
+  messagingSenderId: "711908351848",
+  appId: "1:711908351848:web:a6c1ac4836988c4a7340dd"
 };
 
 // تهيئة Firebase
